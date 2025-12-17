04e00d8b64ee1255130b16794e28cb71c76109d7
Arthur Ribeiro <arthurgrayy@gmail.com>
2025-12-17 07:59:12 -0300

    vercel4

--- api/index.py
+++ api/index.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+# Add the parent directory to sys.path so 'app.py' can be imported
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app import app
 
 def handler(request, context):
